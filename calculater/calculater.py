e94c1b8b504fd423164d6153cdda8f5e392c7a4c
Adhinee <103882598+Adhinee@users.noreply.github.com>
2022-10-26 12:25:30 -0700

    Update calculater.py

--- calculater/calculater.py
+++ calculater/calculater.py
@@ -3,6 +3,7 @@ from ctypes import resize
 from email.mime import image
 from tkinter import *
 from tkinter import messagebox
+from turtle import end_fill
 from PIL import ImageTk, Image
 
 
@@ -27,7 +28,7 @@ def on_enter2(e):
     e.widget['background'] = 'green'
 
 def on_leave2(e):
-    e.widget['background'] = '#2F4F4F'
+    e.widget['background'] = 'red'
 
 p=Label(root,image=img)
 # p = Label(root, text="powered by: Adhinee",font=("arial",10),bg="#2F4F4F",fg="white")
@@ -38,76 +39,151 @@ p.pack()
 
 
 
+# ======================================================================================================================================================================
 
-entry=Entry(root,bg="#E5E5E5",font="Georgia 20",fg="black",bd=5)
-# entry.bind("<Enter>", on_enter)
-# entry.bind("<Leave>", on_leave)
+def add(nmbr):
+    current = entry.get()
+    entry.delete(0, END)
+    entry.insert(0 ,str(current) + str(nmbr) )
+    
+    
+def plus():
+    global firstnmbr
+    first=entry.get()
+    firstnmbr=int(first)
+    entry.delete(0,END)
+
+
+def equal():
+    second=entry.get()
+    global secondnmbr
+    secondnmbr=int(second)
+    entry.delete(0, END)
+    entry.insert(0 , firstnmbr + secondnmbr)
+
+def clear():
+    entry.delete(0 , END)
+
+
+# def delete():
+#     entry.delete(END , )
+
+
+
+
+
+
+
+# ===================================================================------------------------------------------
+
+entry=Entry(root,bg="#2F4F4F",font="Georgia 20",fg="white",bd=5)
+entry.bind("<Enter>", on_enter)
+entry.bind("<Leave>", on_leave)
 entry.pack()
 entry.place(x = 10,y = 10,width=340,height=50)
 
-
-
-
-btn1=Button(root,text="1",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn1=Button(root,text="1",command=lambda : add(1),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn1.bind("<Enter>", on_enter)
 btn1.bind("<Leave>", on_leave)
 btn1.pack()
 btn1.place(x=0,y=100)
 
-btn2=Button(root,text="2",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn2=Button(root,text="2",command=lambda : add(2),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn2.bind("<Enter>", on_enter)
 btn2.bind("<Leave>", on_leave)
 btn2.pack()
 btn2.place(x=120,y=100)
 
-btn3=Button(root,text="3",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn3=Button(root,text="3",command=lambda : add(3),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn3.bind("<Enter>", on_enter)
 btn3.bind("<Leave>", on_leave)
 btn3.pack()
 btn3.place(x=240,y=100)
 
-btn4=Button(root,text="4",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn4=Button(root,text="4",command=lambda : add(4),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn4.bind("<Enter>", on_enter)
 btn4.bind("<Leave>", on_leave)
 btn4.pack()
 btn4.place(x=0,y=150)
 
-btn5=Button(root,text="5",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn5=Button(root,text="5",command=lambda : add(5),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn5.bind("<Enter>", on_enter)
 btn5.bind("<Leave>", on_leave)
 btn5.pack()
 btn5.place(x=120,y=150)
 
-btn6=Button(root,text="6",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn6=Button(root,text="6",command=lambda : add(6),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn6.bind("<Enter>", on_enter)
 btn6.bind("<Leave>", on_leave)
 btn6.pack()
 btn6.place(x=240,y=150)
 
-btn7=Button(root,text="7",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn7=Button(root,text="7",command=lambda : add(7),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn7.bind("<Enter>", on_enter)
 btn7.bind("<Leave>", on_leave)
 btn7.pack()
 btn7.place(x=0,y=200)
 
-btn8=Button(root,text="8",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn8=Button(root,text="8",command=lambda : add(8),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn8.bind("<Enter>", on_enter)
 btn8.bind("<Leave>", on_leave)
 btn8.pack()
 btn8.place(x=120,y=200)
 
 
-btn9=Button(root,text="9",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn9=Button(root,text="9",command=lambda : add(9),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn9.bind("<Enter>", on_enter)
 btn9.bind("<Leave>", on_leave)
 btn9.pack()
 btn9.place(x=240,y=200)
 
-btn0=Button(root,text="0",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white")
+btn0=Button(root,text="0",command=lambda : add(0),font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
 btn0.bind("<Enter>", on_enter)
 btn0.bind("<Leave>", on_leave)
 btn0.pack()
 btn0.place(x=120,y=250)
+
+btndelt=Button(root,text="DEL",font="10",padx="33",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btndelt.bind("<Enter>", on_enter)
+btndelt.bind("<Leave>", on_leave)
+btndelt.pack()
+btndelt.place(x=0,y=250)
+
+btneql=Button(root,command=equal,text="=",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btneql.bind("<Enter>", on_enter)
+btneql.bind("<Leave>", on_leave)
+btneql.pack()
+btneql.place(x=240,y=250)
+
+btnpls=Button(root,command=plus,text="+",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btnpls.bind("<Enter>", on_enter)
+btnpls.bind("<Leave>", on_leave)
+btnpls.pack()
+btnpls.place(x=360,y=100)
+
+btnmins=Button(root,text="-",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btnmins.bind("<Enter>", on_enter)
+btnmins.bind("<Leave>", on_leave)
+btnmins.pack()
+btnmins.place(x=360,y=150)
+
+btnmultpl=Button(root,text="*",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btnmultpl.bind("<Enter>", on_enter)
+btnmultpl.bind("<Leave>", on_leave)
+btnmultpl.pack()
+btnmultpl.place(x=360,y=200)
+
+btndvd=Button(root,text="/",font="10",padx="45",pady="5",bd=4,bg="#2F4F4F",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btndvd.bind("<Enter>", on_enter)
+btndvd.bind("<Leave>", on_leave)
+btndvd.pack()
+btndvd.place(x=360,y=250)
+
+btnclr=Button(root,text="CLEAR",command=clear,font="10",padx="21",pady="5",bd=4,bg="red",fg="white",activebackground="#2F4F4F",activeforeground="white")
+btnclr.bind("<Enter>", on_enter)
+btnclr.bind("<Leave>", on_leave2)
+btnclr.pack()
+btnclr.place(x=360,y=17)
 
 # btn1=Button(root,text="1",font="10",padx="100",pady="5")
 # btn1=Button(root,text="1",font="10",padx="100",pady="5")
@@ -123,7 +199,7 @@ btn0.place(x=120,y=250)
 # btn4.place(x=20,y=0)
 # btn5.place(x=0,y=0)
 # btn6.place(x=20,y=0)
-# btn7.place(x=0,y=0)
+# btn7.place(x=0,y=0 )
 # btn8.place(x=20,y=0)
 # btn9.place(x=0,y=0)
 # btn0.place(x=20,y=0)
